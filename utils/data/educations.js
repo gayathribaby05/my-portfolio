27156ef99cb0ab5b9a75e016a9acdcda9c00253a
Teja Telagathoti <teja2022ai@gmail.com>
2024-09-14 10:37:57 +0530

    data modification

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2021 - Present",
+    institution: "KKR & KSR Institute of Technology and Sciences",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    duration: "2019 - 2021",
+    institution: "Modern Junior College of Arts and Sciences",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    duration: "2008 - 2019",
+    institution: "BRIG High School Chilakaluripet",
   }
 ]
