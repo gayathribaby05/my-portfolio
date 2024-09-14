038a1ce8f1b8c2cdf976dda6852fbc0c72710770
Teja Telagathoti <teja2022ai@gmail.com>
2024-09-14 13:30:19 +0530

    projects data

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,9 +6,9 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
     {
         id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
+        name: 'AI-powered Chatbot for KITS College Website',
+        description: "Me and my team built an AI-powered Chatbot for KITS College Website. I have developed using the Dialogflow, TensorFlow with Python. Built AI assistants using OpenAI's latest model and trained using our dataset. The Chatbot fetches data from User Input and generates the college related information. ",
+        tools: ['TensorFlow', 'DialogFlow', 'Python', 'HTML', 'CSS', 'JS', 'NodeJS',],
         role: 'Backend Developer',
         code: '',
         demo: '',
@@ -16,7 +16,7 @@ export const projectsData = [
     },
     {
         id: 2,
-        name: 'Travel Agency App',
+        name: 'Jalavishudhakara',
         description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
         tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
         role: 'Full Stack Developer',
