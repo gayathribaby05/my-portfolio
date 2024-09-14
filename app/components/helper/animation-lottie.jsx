373ebd5b27f40c0b951d530bbe9bc5633e19534f
Teja Telagathoti <teja2022ai@gmail.com>
2024-09-14 09:41:37 +0530

    kk

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,23 @@
 "use client";
 
-import Lottie from "lottie-react";
+import { useEffect } from 'react';
+import lottie from 'lottie-web';
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
+const AnimationLottie = () => {
+  useEffect(() => {
+    // Ensure this runs only on the client side
+    if (typeof window !== 'undefined') {
+      lottie.loadAnimation({
+        container: document.getElementById('animation'), // Use the document object safely
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'data.json' // Path to your animation file
+      });
     }
-  };
+  }, []);
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  return <div id="animation" />;
 };
 
 export default AnimationLottie;
