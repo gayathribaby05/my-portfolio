1322d50df87551c63a404dc5270a48509cd73aa7
teja IG <teja2022ai@gmail.com>
2025-03-24 18:58:41 +0530

    analytics turned on?

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
+import { Analytics } from "vercel/analytics/react"
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
@@ -35,6 +36,7 @@ export default async function Home() {
       <Education />
       <Blog blogs={blogs} />
       <ContactSection />
+      <Analytics />
     </>
   )
 };
